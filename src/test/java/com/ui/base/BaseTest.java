6fc10c397301e345143add8d7d88bf56d3081f00
praveenbr1988 <praveenbr1988@gmail.com>
2024-06-18 23:11:54 -0400

    Implemented Listeners and Log4j2

--- src/test/java/com/ui/base/BaseTest.java
+++ src/test/java/com/ui/base/BaseTest.java
@@ -1,8 +1,11 @@
 package com.ui.base;
 
+import com.ui.pageObjects.HomePage;
 import com.ui.utils.ConfigurationReader;
 import com.ui.utils.DriverManager;
+import com.ui.utils.LoggerUtil;
 import com.ui.utils.YamlReader;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,6 +14,7 @@ import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
+    protected static final Logger logger = LoggerUtil.getLogger(BaseTest.class);
     protected WebDriver driver;
     //String url = ConfigurationReader.getProperty("app.url");
 
@@ -18,6 +22,7 @@ public class BaseTest {
     @Parameters("browser")
     @BeforeMethod
     public void setUp(@Optional("chrome") String browser) {
+        logger.info("Test setup started");
         String env = ConfigurationReader.getProperty("env");
         YamlReader.setEnvironment(env);
         ConfigurationReader.getEnvConfigValues();
@@ -33,5 +38,6 @@ public class BaseTest {
     @AfterMethod
     public void tearDown() {
         DriverManager.quitDriver();
+        logger.info("Tear down completed");
     }
 }
--- src/test/java/com/ui/tests/HomePageTest.java
+++ src/test/java/com/ui/tests/HomePageTest.java
@@ -2,13 +2,18 @@ package com.ui.tests;
 
 import com.ui.base.BaseTest;
 import com.ui.pageObjects.HomePage;
+import com.ui.utils.LoggerUtil;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomePageTest extends BaseTest {
 
+    private static final Logger logger = LoggerUtil.getLogger(HomePageTest.class);
+
     @Test
     public void testGoogleSearch() {
+        logger.info("Google Search test started");
         HomePage homePage = new HomePage(driver);
         homePage.enterSearchTerm("Selenium WebDriver");
         String title = driver.getTitle();
